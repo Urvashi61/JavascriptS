9756274b4cd9699ee73f40e256e2a86805f0b9d2
Urvashi61 <94282404+Urvashi61@users.noreply.github.com>
2024-10-09 09:40:39 +0000

    variables study

---
+++ basics_01/02_variables.js
@@ -0,0 +1,18 @@
+// there are diffrent types of variable
+/*
+bigInt
+
+symbol=> unique
+number
+boolean
+string
+undefind
+
+non primitive data type
+array
+object
+null
+
+Typeof keyword is used to check the type of data which is used.
+*/
+
